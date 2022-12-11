ff7d66c523e2970e4e54eedd7cd9f310064b66fe
Malak ElKhashab <98717580+malakel-khashab@users.noreply.github.com>
2022-12-11 18:32:07 +0200

    Update Exam.js

--- Schemas/Exam.js
+++ Schemas/Exam.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const ExamSchema = new Schema({
 Exam_ID: {
-    type: String,
+    type: Number,
     required: true,
   },
   Exam_Question_ID: {
@@ -11,15 +11,15 @@ Exam_ID: {
     required: true
   },
   Exam_Grade: {
-    type: String,
+    type: Number,
     required: true,
   },
   Exam_Instructor_ID: {
-    type: String,
+    type: Number,
     required: true
   },
   Exam_Course_ID: {
-    type: String,
+    type: Number,
     required: true,
   },
 }, { timestamps: true });
